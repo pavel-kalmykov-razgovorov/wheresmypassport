e89807b26285514daa1d0fb67beb1114e3008c72
Pavel Razgovorov <paveltrufi@hotmail.com>
2019-04-12 13:32:52 +0200

    refactor: migrate to Spek2

--- src/test/kotlin/com/github/putopavel/PdfTextExtractorUseCaseSpek.kt
+++ src/test/kotlin/com/github/putopavel/PdfTextExtractorUseCaseSpek.kt
@@ -1,22 +1,20 @@
 package com.github.putopavel
 
-import io.micronaut.context.BeanContext
-import org.amshove.kluent.shouldEqual
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import io.micronaut.context.ApplicationContext
+import org.amshove.kluent.`should be equal to`
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 import java.io.FileInputStream
 
 
 internal class PdfTextExtractorUseCaseSpek : Spek({
-    describe("PDF text extractor use case") {
-        val sut = BeanContext.run().getBean(PdfTextExtractorUseCase::class.java)
-        on("extract sample PDF's text") {
+    val sut = ApplicationContext.run().getBean(PdfTextExtractorUseCase::class.java)
+    describe("Extract sample PDF's text") {
+        context("a basic PDF with text only") {
             val stream = FileInputStream("src/test/resources/test.pdf")
             it("should extract all the text") {
                 val extractedText = sut.extractText(stream)
-                extractedText.trim() shouldEqual "This is a test"
+                extractedText.trim() `should be equal to` "This is a test"
             }
         }
     }
--- src/test/kotlin/com/github/putopavel/RemotePdfTextExtractorUseCaseSpek.kt
+++ src/test/kotlin/com/github/putopavel/RemotePdfTextExtractorUseCaseSpek.kt
@@ -1,21 +1,19 @@
 package com.github.putopavel
 
-import io.micronaut.context.BeanContext
-import org.amshove.kluent.shouldContain
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.context
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
+import io.micronaut.context.ApplicationContext
+import org.amshove.kluent.`should contain`
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 import java.net.URL
 
 internal class RemotePdfTextExtractorUseCaseSpek : Spek({
-    describe("Remote PDF text extractor use case") {
-        val sut = BeanContext.run().getBean(RemotePdfTextExtractorUseCase::class.java)
-        context("extract sample PDF's text") {
+    val sut = ApplicationContext.run().getBean(RemotePdfTextExtractorUseCase::class.java)
+    describe("Extract sample remote PDF's text") {
+        context("a URL which points to a PDF") {
             val url = URL("http://www.orimi.com/pdf-test.pdf")
             it("should extract all the text") {
                 val extractedText = sut.extractText(url)
-                extractedText.trim() shouldContain "PDF Test File"
+                extractedText.trim() `should contain` "PDF Test File"
             }
         }
     }
